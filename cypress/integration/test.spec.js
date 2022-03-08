147681733853946da653b5b3fbb08bbfdb633492
the0solitary0cyclist <40065312+the0solitary0cyclist@users.noreply.github.com>
2022-03-08 08:10:38 -0500

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,77 +1,23 @@
-// describe('My First Test', () => {
-//     it('B', () => {
-//        cy.visit('https://api.toodledo.com/3/account/authorize.php?response_type=code&client_id=toodledid&state=false&scope=basic%20tasks%20write')
-//        cy.get('form')
-// //        cy.get('input[name="authorized"]')
-//   //     .should('be.visible')
-//          .should('have.attr', 'href')
-//          .then((href) => {
-//             cy.log(href)
-//             cy.request('POST', href, {
-//                  email: Cypress.env('email'),
-//                  password: Cypress.env('password'),
-//             }).then((response) => {
-//                  // pull out the location redirect
-//                  const loc = response.headers['Location']
-//                  cy.log(loc)
-//                  cy.log(response)
-//             })
-//            })
-         
-//     })
-// })
-
-
-// describe('My First Test', () => {
-//     it('B', () => {
-//     const url = "https://api.toodledo.com/3/account/authorize.php?signout=1&response_type=code&client_id=toodledid&state=false&scope=basic%20tasks%20write%20folders"
-
-    
-//     const urlRedirects = [];
-
-
-//     cy.request('POST', url, {
-//                  email: Cypress.env('email'),
-//                  password: Cypress.env('password'),
-//        }).then((response) => {
-//                cy.on('url:changed', (url) => { urlRedirects.push(url);});
-// //              cy.location().should((loc) => { // comes up blank
-// //    expect(loc.pathname.toString()).to.contain('/home');
-//  });
-//              // pull out the location redirect
-// //              const loc = response.headers
-// //              cy.log(loc)
-// //              expect(JSON.stringify(response)).to.equal('foo')
-//         }).then(() => {
-
-//       expect(urlRedirects).to.have.length(3);
-//       expect(urlRedirects[1]).to.eq('foo');
-//       expect(urlRedirects[2]).to.eq('bar');
-
-//     });
-// })
-
 describe('My First Test', () => {
     it('B', () => {
     const url = "https://api.toodledo.com/3/account/authorize.php?signout=1&response_type=code&client_id=toodledid&state=false&scope=basic%20tasks%20write%20folders"
 
     
 //     const urlRedirects = [];
-
-
-    cy.request('POST', url, {
+    
+    cy.request({
+  method: 'POST',
+  url: url, 
+  form: true, // indicates the body should be form urlencoded and sets Content-Type: application/x-www-form-urlencoded headers
+  followRedirect: false, // turn off following redirects
+  body: {
                  email: Cypress.env('email'),
                  password: Cypress.env('password'),
-       })
-       .then((response) => {
-                  expect(response.status).to.equal(302)
-                  expect(response.headers).to.equal('foo')
-//                cy.on('url:changed', (url) => { urlRedirects.push(url);});
+  },
+}).then((resp) => {
+           // redirect status code is 302
+           expect(resp.status).to.eq(302)
+           expect(resp.redirectedToUrl).to.eq('http://localhost:8082/unauthorized')
+         })
         })
-//         .then(() =>{
-//             expect(urlRedirects).to.have.length(3);
-//             expect(urlRedirects[1]).to.eq('foo');
-//             expect(urlRedirects[2]).to.eq('bar');
-//         })
     })
-})
