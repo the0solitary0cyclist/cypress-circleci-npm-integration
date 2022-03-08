213750a5a62bbfd6bdff1a921e5193449c65ab3b
the0solitary0cyclist <40065312+the0solitary0cyclist@users.noreply.github.com>
2022-03-08 06:32:00 -0500

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -16,6 +16,7 @@
 //          cy.get('input[name="authorized"]')
 //          .should('be.visible')
 //          .click()
+
 //   })
 // })
 
@@ -43,6 +44,12 @@ describe('My First Test', () => {
          cy.get('input[name="authorized"]')
          .should('be.visible')
          .click()
+         .then((xhrs) => {
+              console.log(xhrs[0])
+    // xhrs is an array of network call objects
+//     expect(xhrs[0], 'first request status').to.have.property('status', 201)
+//     expect(xhrs[1], 'second request status').to.have.property('status', 201)
+     })
   })
 })
 
