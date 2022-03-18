bee0827e49cf88b557e72e55ac75adcb3db8331c
the0solitary0cyclist <40065312+the0solitary0cyclist@users.noreply.github.com>
2022-03-18 09:40:28 -0400

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -22,8 +22,12 @@ describe('My First Test', () => {
          cy.get('input[name="authorized"]')
          .should('be.visible')
          .click()
-         .then((xhrs) => {
-              cy.log(xhrs)
+         cy.url().should(
+      'be.equal',
+      'https://toodledid.herokuapp.com/profile'
+    )
+//          .then((xhrs) => {
+//               cy.log(xhrs)
 //               console.log(xhrs[0])
     // xhrs is an array of network call objects
 //     expect(xhrs[0], 'first request status').to.have.property('status', 201)
